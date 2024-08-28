8423332d40466ab681e0a760b00a6816286a2f9a
Abongile Mavuma <mavumaabongile@gmail.com>
2024-08-28 11:59:32 +0200

    Added project section

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const nameElement = document.getElementById('name');
+    
+    let names = ['Vuyelwa', 'Mavooma'];
+    let index = 0;
+
+    function toggleName() {
+        nameElement.textContent = names[index];
+        index = (index + 1) % names.length; // Switch between names
+    }
+
+    // Initial call
+    toggleName();
+    
+    // Change name every 2 seconds
+    setInterval(toggleName, 2000);
+});
